64a8d9ba614ca4602522912c1f4a153b797b5787
hieu <htrinhtrieu@pentalog.com>
2022-02-10 20:34:45 +0700

    123. Make template links working in Symfony - part 2

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -31,4 +31,35 @@ class FrontController extends AbstractController
     {
         return $this->render('front/video_details.html.twig');
     }
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResults()
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
