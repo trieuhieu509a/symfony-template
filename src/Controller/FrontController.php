5270e1af9fd89c55822e0d6cb931669fff7fc642
hieu <htrinhtrieu@pentalog.com>
2022-02-23 23:44:04 +0700

    133. Category class for front-end

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Utils\CategoryTreeFrontPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +21,9 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}", name="video_list")
      */
-    public function videoList()
+    public function videoList($id, CategoryTreeFrontPage $categories)
     {
+        dump($categories);
         return $this->render('front/video_list.html.twig');
     }
 
