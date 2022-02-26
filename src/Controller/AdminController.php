63d1ce67fd36b3b57aa833d5e3f754c46a33076b
hieu <htrinhtrieu@pentalog.com>
2022-02-26 23:39:46 +0700

    140. Edit category in admin panel - part 2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Utils\CategoryTreeAdminList;
 use App\Utils\CategoryTreeAdminOptionList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,14 +26,25 @@ class AdminController extends AbstractController
 
 
     /**
-     * @Route("/categories", name="categories")
+     * @Route("/categories", name="categories", methods={"GET","POST"})
      */
-    public function categories(CategoryTreeAdminList $categories)
+    public function categories(CategoryTreeAdminList $categories, Request $request)
     {
         $categories->getCategoryList($categories->buildTree());
 
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // save category
+            dd('valid');
+        }
+
         return $this->render('admin/categories.html.twig',[
-            'categories'=>$categories->categorylist
+            'categories'=>$categories->categorylist,
+            'form'=>$form->createView()
         ]);
     }
 
