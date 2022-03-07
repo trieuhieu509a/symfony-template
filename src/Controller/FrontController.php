4ec4e3a9ea77593383e1aa8db7f5faa7a47a740a
hieu <htrinhtrieu@pentalog.com>
2022-03-08 00:03:44 +0700

    165. Symfony login system - part 4

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
 {
@@ -89,9 +90,19 @@ class FrontController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login()
+    public function login(AuthenticationUtils $helper)
     {
-        return $this->render('front/login.html.twig');
+        return $this->render('front/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout() : void
+    {
+        throw new \Exception('This should never be reached!');
     }
 
     /**
