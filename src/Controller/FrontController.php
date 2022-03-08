828c55387efc75f3d2fadcadede09bfe1b81c5f3
hieu <htrinhtrieu@pentalog.com>
2022-03-08 23:35:47 +0700

    175. User registration - part 3

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -12,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
@@ -84,15 +86,28 @@ class FrontController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request)
+    public function register(UserPasswordEncoderInterface $password_encoder, Request $request)
     {
-
-        $user = new User();
+        $user = new User;
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid())
         {
-            dd('registering user ...');
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUserAutomatically($user, $password);
+
+            return $this->redirectToRoute('admin_main_page');
         }
         return $this->render('front/register.html.twig',['form'=>$form->createView()]);
     }
@@ -131,5 +146,17 @@ class FrontController extends AbstractController
         return $this->render('front/_main_categories.html.twig',[
             'categories'=>$categories
         ]);
+    }
+
+    private function loginUserAutomatically($user, $password)
+    {
+        $token = new UsernamePasswordToken(
+            $user,
+            $password,
+            'main', // security.yaml
+            $user->getRoles()
+        );
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_main',serialize($token));
     }
 }
