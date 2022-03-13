530ed363018826d5c42737b045b1191a56f8b174
hieu <htrinhtrieu@pentalog.com>
2022-03-13 23:34:44 +0700

    222. Superadmin profile

--- src/Controller/Admin/Superadmin/SuperAdminController.php
+++ src/Controller/Admin/Superadmin/SuperAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin\Superadmin;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +25,22 @@ class SuperAdminController extends AbstractController
      */
     public function users()
     {
-        return $this->render('admin/users.html.twig');
+
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findBy([], ['name' => 'ASC']);
+        return $this->render('admin/users.html.twig',['users'=>$users]);
     }
+
+    /**
+     * @Route("/delete-user/{user}", name="delete_user")
+     */
+    public function deleteUser(User $user)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($user);
+        $manager->flush();
+
+        return $this->redirectToRoute('users');
+    }
+
 }
