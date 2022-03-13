f7665849e698b2ffefe29274f6ecad7a6e1f284f
hieu <htrinhtrieu@pentalog.com>
2022-03-13 22:03:43 +0700

    216. Delete admin profile

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -24,6 +24,22 @@ class MainController extends AbstractController
     }
 
     /**
+     * @Route("/delete-account", name="delete_account")
+     */
+    public function deleteAccount()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->find($this->getUser());
+
+        $em->remove($user);
+        $em->flush();
+
+        session_destroy();
+
+        return $this->redirectToRoute('main_page');
+    }
+
+    /**
      * @Route("/cancel-plan", name="cancel_plan")
      */
     public function cancelPlan()
